caeb55957955c8c20d7016159cfe7740fe6ebf18
Christian Donoso <christ.donoso@gmail.com>
2025-01-22 17:19:42 -0300

    commit con inicio de modelos de rubricas

--- rubricas/models.py
+++ rubricas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from main.models import Estudiante
 # Create your models here.
+
+
+class Internado(models.Model):
+    estudiante = models.ForeignKey(Estudiante,on_delete=models.CASCADE)
+    orden_rotativa = models.IntegerField()
+    lugar = models.CharField(max_length=100)
+    profesor_guia  = models.CharField(max_length=50)
+    
